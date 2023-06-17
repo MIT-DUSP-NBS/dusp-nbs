7517bda3cc47af384b292202fa1ca8e01658c4a0
Dgoat724236 <dtemkin@mit.edu>
2023-06-17 10:45:02 -0400

    Test dashboard

--- myapp/app.py
+++ myapp/app.py
@@ -1,17 +1,30 @@
 from shiny import App, render, ui
+from shinywidgets import output_widget, render_widget
+import shinyswatch
+from ipyleaflet import Map
 
 app_ui = ui.page_fluid(
-    ui.h2("Hello Shiny!"),
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    shinyswatch.theme.zephyr(),
+    ui.panel_title("Nature-Based Solutions Dashboard to Support Carbon Neutrality in Major EU Cities"),
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.h2("Test!"),
+            ui.p("This is where all the sliders and things to configure the map will go :)")
+        ),
+        ui.panel_main(
+            output_widget("map")
+        ),
+    ),
+    
 )
 
 
 def server(input, output, session):
     @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @render_widget
+    def map():
+        m = Map()
+        return m
 
 
 app = App(app_ui, server)
