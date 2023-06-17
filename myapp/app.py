8c5ae582fca6d593be10696233aaa43b5eab54a0
Dgoat724236 <dtemkin@mit.edu>
2023-06-17 11:51:58 -0400

    Change things for testing purposes

--- myapp/app.py
+++ myapp/app.py
@@ -1,28 +1,31 @@
 from shiny import App, render, ui
-from shinywidgets import output_widget, render_widget
-from ipyleaflet import Map
+from shinywidgets import register_widget, output_widget
+from ipyleaflet import Map, basemaps
 
 app_ui = ui.page_fluid(
     ui.panel_title("Nature-Based Solutions Dashboard to Support Carbon Neutrality in Major EU Cities"),
     ui.layout_sidebar(
         ui.panel_sidebar(
             ui.h2("Test!"),
-            ui.p("This is where all the sliders and things to configure the map will go :)")
+            ui.p("This is where all the sliders and things to configure the map will go :)"),
         ),
         ui.panel_main(
-            output_widget("map")
+            output_widget("m")
         ),
-    ),
-    
+    )
 )
 
 
 def server(input, output, session):
-    @output
-    @render_widget
-    def map():
-        m = Map()
-        return m
+    center = [38.128, 2.588] # FOR TESTING
+    zoom = 5 # FOR TESTING
+    m = Map(
+        basemap=basemaps.Gaode.Satellite,
+        center=center,
+        zoom=zoom,
+    )
+    register_widget("m", m)
+
 
 
 app = App(app_ui, server)
