8cca3b6d567cf45c3cd7a059357c2ab808dd0798
Dgoat724236 <dtemkin@mit.edu>
2023-06-19 17:08:48 -0400

    More formatting

--- myapp/app.py
+++ myapp/app.py
@@ -1,16 +1,17 @@
 from ipyleaflet import Map, basemaps
-from shiny import App, render, ui
+from shiny import App, ui
 from shinywidgets import output_widget, register_widget
 
 app_ui = ui.page_fluid(
     ui.panel_title(
-        "Nature-Based Solutions Dashboard to Support Carbon Neutrality in Major EU Cities"
+        "Nature-Based Solutions Dashboard "
+        "to Support Carbon Neutrality in Major EU Cities"
     ),
     ui.layout_sidebar(
         ui.panel_sidebar(
             ui.h2("Test!"),
             ui.p(
-                "This is where all the sliders and things to configure the map will go :)"
+                "This is where all the sliders and things to configure the map will go"
             ),
         ),
         ui.panel_main(output_widget("map")),
