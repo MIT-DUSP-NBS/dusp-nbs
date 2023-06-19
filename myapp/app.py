ca9e25af509ce355a0f7d2d4b9b3c8a1fbb0bd0b
Dgoat724236 <dtemkin@mit.edu>
2023-06-19 16:47:49 -0400

    Formatting

--- myapp/app.py
+++ myapp/app.py
@@ -1,31 +1,32 @@
-from shiny import App, render, ui
-from shinywidgets import register_widget, output_widget
 from ipyleaflet import Map, basemaps
+from shiny import App, render, ui
+from shinywidgets import output_widget, register_widget
 
 app_ui = ui.page_fluid(
-    ui.panel_title("Nature-Based Solutions Dashboard to Support Carbon Neutrality in Major EU Cities"),
+    ui.panel_title(
+        "Nature-Based Solutions Dashboard to Support Carbon Neutrality in Major EU Cities"
+    ),
     ui.layout_sidebar(
         ui.panel_sidebar(
             ui.h2("Test!"),
-            ui.p("This is where all the sliders and things to configure the map will go :)"),
+            ui.p(
+                "This is where all the sliders and things to configure the map will go :)"
+            ),
         ),
-        ui.panel_main(
-            output_widget("map")
-        ),
-    )
+        ui.panel_main(output_widget("map")),
+    ),
 )
 
 
 def server(input, output, session):
-    center = [38.128, 2.588] # FOR TESTING
-    zoom = 5 # FOR TESTING
+    center = [38.128, 2.588]  # FOR TESTING
+    zoom = 5  # FOR TESTING
     map = Map(
         basemap=basemaps.Gaode.Satellite,
         center=center,
         zoom=zoom,
     )
     register_widget("map", map)
-
 
 
 app = App(app_ui, server)
