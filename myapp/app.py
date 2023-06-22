8adddcbb000da0a1abe3f65b60ddc1efd8ac0cac
Dgoat724236 <dtemkin@mit.edu>
2023-06-22 01:27:50 -0400

    Working on GDAL...

--- myapp/app.py
+++ myapp/app.py
@@ -37,6 +37,53 @@ app_ui = ui.page_navbar(
 def server(input, output, session):
     m = Map(basemap=basemaps.Esri.WorldImagery, zoom=0)
 
+    # residential_path = "res.tif"
+    # industrial_path = "ind.tif"
+
+    # residential_raster = gdal.Open(str(assets_dir / residential_path))
+    # industrial_raster = gdal.Open(str(assets_dir / industrial_path))
+
+    # residential_transformer = Transformer.from_crs(
+    #     residential_raster.GetProjection(), "epsg:3857"
+    # )
+    # industrial_transformer = Transformer.from_crs(
+    #     residential_raster.GetProjection(), "epsg:3857"
+    # )
+
+    # rx1, ry1 = residential_transformer.transform(
+    #     residential_raster.bounds[0], residential_raster.bounds[1]
+    # )
+    # rx2, ry2 = residential_transformer.transform(
+    #     residential_raster.bounds[2], residential_raster.bounds[3]
+    # )
+
+    # ix1, iy1 = industrial_transformer.transform(
+    #     industrial_raster.bounds[0], industrial_raster.bounds[1]
+    # )
+    # ix2, iy2 = industrial_transformer.transform(
+    #     industrial_raster.bounds[2], industrial_raster.bounds[3]
+    # )
+
+    # rbounds = ((ry1, rx1), (ry2, rx2))
+    # ibounds = ((iy1, ix1), (iy2, ix2))
+
+    # rimage_map = Image.fromarray(residential_raster.read(1))
+    # rimage_map = rimage_map.convert("L")
+
+    # iimage_map = Image.fromarray(industrial_raster.read(1))
+    # iimage_map = iimage_map.convert("L")
+
+    # rimage_map.save("%s.png" % residential_path)
+    # iimage_map.save("%s.png" % industrial_path)
+
+    # residential = ImageOverlay(
+    #     url="%s.png" % residential_path, name=residential_path, bounds=rbounds
+    # )
+
+    # industrial = ImageOverlay(
+    #     url="%s.png" % industrial_path, name=industrial_path, bounds=ibounds
+    # )
+
     residential = LocalTileLayer(path="res/{z}/{x}/{y}.png")
     industrial = LocalTileLayer(path="ind/{z}/{x}/{y}.png")
 
