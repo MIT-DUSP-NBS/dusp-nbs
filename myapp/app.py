4278aa183e9f0db274b23f58815425ff775dddde
Dgoat724236 <dtemkin@mit.edu>
2023-06-27 01:53:47 -0400

    Minor changes :3

--- myapp/app.py
+++ myapp/app.py
@@ -1,7 +1,7 @@
 from shiny import App, ui, reactive
 from shinywidgets import output_widget, register_widget
 import shinyswatch
-from ipyleaflet import Map, basemaps, LocalTileLayer
+from ipyleaflet import Map, basemaps, LocalTileLayer, Layer
 from pathlib import Path
 
 # try:
@@ -17,7 +17,7 @@ emissions_map = ui.layout_sidebar(
         ui.input_radio_buttons(
             "emissions",
             label="Select the desired emission properties",
-            choices=["residential", "industrial"],
+            choices=["Residential", "Industrial"],
         ),
     ),
     ui.panel_main(output_widget("map")),
@@ -35,7 +35,7 @@ app_ui = ui.page_navbar(
 
 def server(input, output, session):
     m = Map(
-        basemap=basemaps.Esri.WorldImagery,
+        basemap=basemaps.Esri.WorldImagery,  # type: ignore
         zoom=9,
         center=(59.3293, 18.0686),
         max_zoom=13,
@@ -43,20 +43,24 @@ def server(input, output, session):
 
     residential = LocalTileLayer(path="res/{z}/{x}/{y}.png")
     industrial = LocalTileLayer(path="ind/{z}/{x}/{y}.png")
+    empty_layer = Layer()
 
-    m.add_layer(residential)
+    m.add_layer(empty_layer)
 
     register_widget("map", m)
 
     @reactive.Effect()
     def _():
         input_emissions = input.emissions()
-        if input_emissions == "residential":
-            if m.layers[1] == industrial:
-                m.substitute_layer(industrial, residential)
-        elif input_emissions == "industrial":
-            if m.layers[1] == residential:
-                m.substitute_layer(residential, industrial)
+        layers = tuple(m.layers)  # type: ignore
+
+        match input_emissions:
+            case "Residential":
+                m.substitute_layer(layers[-1], residential)
+            case "Industrial":
+                m.substitute_layer(layers[-1], industrial)
+            case _:
+                m.substitute_layer(layers[-1], empty_layer)
 
 
 app = App(app_ui, server, static_assets=assets_dir)
