32da256994c331b08db7a5f298404c16c7aed3e7
Dgoat724236 <dtemkin@mit.edu>
2023-06-30 19:01:23 -0400

    Begin implementation map

--- myapp/app.py
+++ myapp/app.py
@@ -14,11 +14,30 @@ emissions_map = ui.layout_sidebar(
             choices=["Residential", "Industrial"],
         ),
     ),
-    ui.panel_main(output_widget("map")),
+    ui.panel_main(output_widget("map_emissions")),
+)
+
+implementation_visualization = ui.layout_sidebar(
+    ui.panel_sidebar(
+        ui.h2("Implementation Visualization"),
+        ui.input_checkbox_group(
+            "implementation",
+            label="Select the desired green infrastructure implementations:",
+            choices=[
+                "Green Infrastructure",
+                "Green Buildings",
+                "Street Trees",
+                "Urban Green Areas",
+                "Greenbelt",
+            ],
+        ),
+    ),
+    ui.panel_main(output_widget("map_implementation")),
 )
 
 app_ui = ui.page_navbar(
     ui.nav("Emissions Map", emissions_map),
+    ui.nav("Implementation Visualization", implementation_visualization),
     title="Nature-Based Solutions Dashboard",
     inverse=True,
     window_title="Nature-Based Solutions Dashboard",
@@ -26,7 +45,14 @@ app_ui = ui.page_navbar(
 
 
 def server(input, output, session):
-    m = Map(
+    map_emissions = Map(
+        basemap=basemaps.Esri.WorldImagery,  # type: ignore
+        zoom=9,
+        center=(59.3293, 18.0686),
+        max_zoom=13,
+    )
+
+    map_implementation = Map(
         basemap=basemaps.Esri.WorldImagery,  # type: ignore
         zoom=9,
         center=(59.3293, 18.0686),
@@ -54,22 +80,31 @@ def server(input, output, session):
 
     empty_layer = Layer()
 
-    m.add_layer(empty_layer)
+    map_emissions.add_layer(empty_layer)
+    map_implementation.add_layer(empty_layer)
 
-    register_widget("map", m)
+    register_widget("map_emissions", map_emissions)
+    register_widget("map_implementation", map_implementation)
 
     @reactive.Effect()
-    def _():
+    def emmissions():
         input_emissions = input.emissions()
-        layers = tuple(m.layers)  # type: ignore
+        layers = tuple(map_emissions.layers)  # type: ignore
 
         match input_emissions:
             case "Residential":
-                m.substitute_layer(layers[-1], residential)
+                map_emissions.substitute_layer(layers[-1], residential)
             case "Industrial":
-                m.substitute_layer(layers[-1], industrial)
+                map_emissions.substitute_layer(layers[-1], industrial)
             case _:
-                m.substitute_layer(layers[-1], empty_layer)
+                map_emissions.substitute_layer(layers[-1], empty_layer)
+
+    @reactive.Effect()
+    def implementation():
+        input_implementation = input.implementation()
+        layers = tuple(map_implementation.layers)  # type: ignore
+
+        # TODO: Add layer implementation and stuff
 
 
 app = App(app_ui, server, static_assets=assets_dir)
