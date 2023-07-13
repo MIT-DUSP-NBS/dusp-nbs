fbf1dc0c01735da8ddc9d37f4737cd3ac64af03f
Dgoat724236 <dtemkin@mit.edu>
2023-07-13 18:06:30 -0400

    Add description tabs

--- myapp/app.py
+++ myapp/app.py
@@ -8,16 +8,43 @@ from shinywidgets import output_widget, register_widget
 
 assets_dir = Path(__file__).parent / "assets"
 
-emissions_map = experimental.ui.layout_sidebar(
-    experimental.ui.sidebar(
-        ui.h2("Emission Map Visualization"),
-        ui.input_radio_buttons(
-            "emissions",
-            label="Select the desired emission properties:",
-            choices=["Residential", "Industrial"],
-        ),
+# emissions_map = experimental.ui.layout_sidebar(
+#     experimental.ui.sidebar(
+#         ui.h2("Emission Map Visualization"),
+#         ui.input_radio_buttons(
+#             "emissions",
+#             label="Select the desired emission properties:",
+#             choices=["Residential", "Industrial"],
+#         ),
+#     ),
+#     experimental.ui.as_fill_item(output_widget("map_emissions")),
+# )
+
+overview = (
+    ui.h1("Overview"),
+    ui.p("Urban areas contribute more than 60% of global greenhouse gas (GHG) emissions, through residential, commercial, and transportation activities. Nature-based solutions (NbS) are increasingly being adopted by cities worldwide to enhance carbon sequestration, offsets emissions, and promotes sustainable land management practices, thus contributing to global climate change mitigation efforts."),
+    ui.p("Realistic NbS implementation plans toward carbon neutrality, such as restoring natural ecosystems and increasing urban green resources,  need to be both effective in mitigating carbon emissions at the global level and suitable for the socio-economic and physical conditions at the local level. Prioritizing suitable sites and solutions can enhance the long-term viability of NbS. In our research, we have explored a systematic approach to spatially prioritizing different types of NbS implementations in multiple major EU cities."),
+    ui.p("The motivation for developing this tool is to offer the necessary flexibility for NbS planning, by enabling users to interact and iterate through our spatial allocation processes. Successful adoption of NbS and realization of their functionality requires a holistic and collaborative planning approach that incorporates stakeholders across scales and disciplines. This platform aims to serve as a point of departure to facilitate the identifying suitable interventions and enhancing the awareness of NbS opportunities in urban settings."),
+    ui.tags.figure(
+        {"class": "figure"},
+        ui.img({"class": "figure-img img-fluid"}, src="figures/overview.png"),
+        ui.tags.figcaption({"class": "figure-caption"}, "Potential pathways of NbS to reduce emissions")
+    )
+)
+
+nbs_allocation = (
+    ui.h1("NbS Spatial Allocation"),
+    ui.p("We selected five types of NbS (green infrastructure (GI), street trees & green pavements, urban green spaces & agriculture, habitat preservation & remediation, and green buildings) as our study objectives. From established definitions of NbS in the literature, we identified the level of benefit of different types of NbS at different urban settings and synthesized quantitative indicators to describe the impact of NbS on sectoral carbon emissions."),
+    ui.tags.figure(
+        {"class": "figure"},
+        ui.img({"class": "figure-img img-fluid"}, src="figures/nbs_1.png"),
+        ui.tags.figcaption({"class": "figure-caption"}, "Comparative summary of NbS impacts to sectoral emissions")
     ),
-    experimental.ui.as_fill_item(output_widget("map_emissions")),
+    ui.p("The NbS implementations were spatially allocated based on three major factors: the sectoral carbon emission, potential NbS benefits, and the local context of each location. For example, street trees & green pavements as an NbS to promote walking and cycling should ideally be located along city roads in high-density urban areas, while preserved habitats should be located at the urban fringe where new urban developments are likely to occur. We have developed practical principles and criteria that systematically guide the spatial allocations of each type of NbS."),
+    ui.tags.figure(
+        {"class": "figure"},
+        ui.img({"class": "figure-img img-fluid"}, src="figures/nbs_2.png"),
+    ),
 )
 
 implementation_visualization = experimental.ui.layout_sidebar(
@@ -38,9 +65,82 @@ implementation_visualization = experimental.ui.layout_sidebar(
     experimental.ui.as_fill_item(output_widget("map_implementation")),
 )
 
+interactive = None
+
+about = (
+    ui.tags.table(
+        {"class": "table"},
+        ui.tags.caption("Key variables and data sources used in the analysis"),
+        ui.tags.thead(
+            ui.tags.tr(
+                ui.tags.th({"scope": "col"}, "Type"),
+                ui.tags.th({"scope": "col"}, "Description"),
+                ui.tags.th({"scope": "col"}, "Data Source"),
+                ui.tags.th({"scope": "col"}, "Year"),
+                ui.tags.th({"scope": "col"}, "Available"),
+            )
+        ),
+        ui.tags.tbody(
+            ui.tags.tr(
+                ui.tags.td("Emissions"),
+                ui.tags.td("CO2 emissions by sector"),
+                ui.tags.td("Global Carbon Grid"),
+                ui.tags.td("2019"),
+                ui.tags.td(ui.a("Click here!", href="http://gidmodel.org.cn/?page_id=1425", target="_blank")),
+            ),
+            ui.tags.tr(
+                ui.tags.td("Residential development"),
+                ui.tags.td("Urban fabric density"),
+                ui.tags.td("Urban Atlas via Copernicus"),
+                ui.tags.td("2018"),
+                ui.tags.td(ui.a("Click here!", href="https://land.copernicus.eu/local/urban-atlas/urban-atlas-2018", target="_blank")),
+            ),
+            ui.tags.tr(
+                ui.tags.td("Population"),
+                ui.tags.td("Population per cell"),
+                ui.tags.td("Global Carbon Grid"),
+                ui.tags.td("2019"),
+                ui.tags.td(ui.a("Click here!", href="https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/population-distribution-demography/geostat", target="_blank")),
+            ),
+            ui.tags.tr(
+                ui.tags.td("Building density"),
+                ui.tags.td("Urban fabric density classifications"),
+                ui.tags.td("Urban Atlas via Copernicus"),
+                ui.tags.td("2018"),
+                ui.tags.td(ui.a("Click here!", href="https://land.copernicus.eu/local/urban-atlas/urban-atlas-2018", target="_blank")),
+            ),
+            ui.tags.tr(
+                ui.tags.td("Transportation"),
+                ui.tags.td("Roadway network with classification and speed"),
+                ui.tags.td("OpenStreet Map"),
+                ui.tags.td("2017"),
+                ui.tags.td(ui.a("Click here!", href="https://www.openstreetmap.org/#map=5/62.994/17.637", target="_blank")),
+            ),
+            ui.tags.tr(
+                ui.tags.td("Industry"),
+                ui.tags.td("Industrial, commercial, public, military and private units"),
+                ui.tags.td("Urban Atlas via Copernicus"),
+                ui.tags.td("2018"),
+                ui.tags.td(ui.a("Click here!", href="https://land.copernicus.eu/local/urban-atlas/urban-atlas-2018", target="_blank")),
+            ),
+            ui.tags.tr(
+                ui.tags.td("City boundaries"),
+                ui.tags.td("Functional urban area boundaries"),
+                ui.tags.td("European Commission"),
+                ui.tags.td("2015"),
+                ui.tags.td(ui.a("Click here!", href="https://data.jrc.ec.europa.eu/dataset/jrc-luisa-ui-boundaries-fua", target="_blank")),
+            ),
+        )
+    )
+)
+
 app_ui = experimental.ui.page_navbar(
-    ui.nav("Emissions Map", emissions_map),
+    ui.nav("Overview", overview),
+    ui.nav("NbS Spatial Allocation", nbs_allocation),
+    # ui.nav("Emissions Map", emissions_map),
     ui.nav("Implementation Visualization", implementation_visualization),
+    ui.nav("Interactive NbS Planning", interactive),
+    ui.nav("About the Tool", about),
     ui.nav_spacer(),
     ui.nav_control(
         ui.a(
@@ -70,12 +170,12 @@ app_ui = experimental.ui.page_navbar(
 def server(input, output, session):
     map_layout = Layout(height="96%")
 
-    residential = ipyl.LocalTileLayer(
-        path="emissions/res/{z}/{x}/{y}.png", name="Residential"
-    )
-    industrial = ipyl.LocalTileLayer(
-        path="emissions/ind/{z}/{x}/{y}.png", name="Industrial"
-    )
+    # residential = ipyl.LocalTileLayer(
+    #     path="emissions/res/{z}/{x}/{y}.png", name="Residential"
+    # )
+    # industrial = ipyl.LocalTileLayer(
+    #     path="emissions/ind/{z}/{x}/{y}.png", name="Industrial"
+    # )
 
     gbi = ipyl.LocalTileLayer(path="implementation/gbi/{z}/{x}/{y}.png", name="GBI")
     gbi_greenbelt = ipyl.LocalTileLayer(
@@ -136,14 +236,14 @@ def server(input, output, session):
         style={"color": "white", "fillOpacity": "0.00"},
     )
 
-    map_emissions = ipyl.Map(
-        basemap=ipyl.basemaps.Esri.WorldImagery,  # type: ignore
-        zoom=9,
-        center=(59.3293, 18.0686),
-        max_zoom=13,
-        scroll_wheel_zoom=True,
-        layout=map_layout,
-    )
+    # map_emissions = ipyl.Map(
+    #     basemap=ipyl.basemaps.Esri.WorldImagery,  # type: ignore
+    #     zoom=9,
+    #     center=(59.3293, 18.0686),
+    #     max_zoom=13,
+    #     scroll_wheel_zoom=True,
+    #     layout=map_layout,
+    # )
 
     map_implementation = ipyl.Map(
         basemap=ipyl.basemaps.Esri.WorldImagery,  # type: ignore
@@ -158,10 +258,10 @@ def server(input, output, session):
 
     map_implementation.add(geo_stockholm)
 
-    map_emissions.add(empty_layer)
+    # map_emissions.add(empty_layer)
     map_implementation.add(empty_layer)
 
-    register_widget("map_emissions", map_emissions)
+    # register_widget("map_emissions", map_emissions)
     register_widget("map_implementation", map_implementation)
 
     start_modal = ui.modal(
@@ -175,18 +275,18 @@ def server(input, output, session):
 
     ui.modal_show(start_modal)
 
-    @reactive.Effect()
-    def emmissions():
-        input_emissions = input.emissions()
-        layers = tuple(map_emissions.layers)  # type: ignore
+    # @reactive.Effect()
+    # def emmissions():
+    #     input_emissions = input.emissions()
+    #     layers = tuple(map_emissions.layers)  # type: ignore
 
-        match input_emissions:
-            case "Residential":
-                map_emissions.substitute(layers[-1], residential)
-            case "Industrial":
-                map_emissions.substitute(layers[-1], industrial)
-            case _:
-                map_emissions.substitute(layers[-1], empty_layer)
+    #     match input_emissions:
+    #         case "Residential":
+    #             map_emissions.substitute(layers[-1], residential)
+    #         case "Industrial":
+    #             map_emissions.substitute(layers[-1], industrial)
+    #         case _:
+    #             map_emissions.substitute(layers[-1], empty_layer)
 
     @reactive.Effect()
     def implementation():
