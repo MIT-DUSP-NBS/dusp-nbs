8ce81f4fd8c8eebd69ff86e33ff0d1ba51ef34df
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-03-20 15:33:48 -0400

    start adding multiple cities

--- src/sections/Visualization/Visualization.tsx
+++ src/sections/Visualization/Visualization.tsx
@@ -1,5 +1,5 @@
 import { useState, forwardRef, ForwardedRef, useEffect, useMemo } from 'react';
-import { Paper, Checkbox, Switch, Space } from '@mantine/core';
+import { Paper, Checkbox, Switch, Space, Select, ComboboxItem } from '@mantine/core';
 import { RMap, RLayerTile, RLayerVector, RStyle } from 'rlayers';
 import { Feature } from 'ol';
 import { Geometry } from 'ol/geom';
@@ -13,10 +13,11 @@ import {
   RPinchZoom,
 } from 'rlayers/interaction';
 
-import stockholmBoundary from '../../assets/county.json';
 import 'ol/ol.css';
+import { Coordinate } from 'ol/coordinate';
 
-const map_layers = import.meta.glob('../../assets/map_layers/*.json');
+const map_layers = import.meta.glob('../../assets/map_layers/*/*.json');
+const boundaries = import.meta.glob('../../assets/boundaries/*.json', { eager: true });
 
 const data = [
   {
@@ -51,6 +52,20 @@ const data = [
   },
 ];
 
+interface CitiesType extends ComboboxItem {
+  newView?: { center: Coordinate; zoom: number };
+}
+
+const cities: CitiesType[] = [
+  {
+    label: 'Stockholm',
+    value: 'stockholm',
+    newView: { center: fromLonLat([18.0686, 59.3293]), zoom: 9 },
+  },
+  { label: 'Vienna', value: 'vienna', disabled: true },
+  { label: 'Madrid', value: 'madrid', disabled: true },
+];
+
 const colors = {
   GBI: 'lime',
   green_buildings: 'cyan',
@@ -59,7 +74,9 @@ const colors = {
   urban_green: 'orange',
 };
 
-const VisualizationLayers = ({ layers }: { layers: string[] }) => {
+const initial = { center: fromLonLat([7, 50]), zoom: 5 };
+
+const VisualizationLayers = ({ layers, city }: { layers: string[]; city: string }) => {
   const [imports, setImports] = useState<Record<string, unknown>>({});
 
   useEffect(() => {
@@ -68,7 +85,7 @@ const VisualizationLayers = ({ layers }: { layers: string[] }) => {
         layers
           .filter((layer) => !Object.keys(imports).includes(layer))
           .map((value) =>
-            map_layers[`../../assets/map_layers/${value}.json`]().then((importVal) => {
+            map_layers[`../../assets/map_layers/${city}/${value}.json`]().then((importVal) => {
               const returnObj: Record<string, unknown> = {};
               returnObj[value] = importVal;
               return returnObj;
@@ -83,7 +100,7 @@ const VisualizationLayers = ({ layers }: { layers: string[] }) => {
         });
       })
       .catch(() => {});
-  }, [imports, layers]);
+  }, [city, imports, layers]);
 
   return useMemo(
     () =>
@@ -109,12 +126,33 @@ const VisualizationLayers = ({ layers }: { layers: string[] }) => {
   );
 };
 
-const stockholmCenter = fromLonLat([18.0686, 59.3293]);
+const BounadryLayer = ({ city }: { city: ComboboxItem }) => {
+  const boundary = boundaries[`../../assets/boundaries/${city.value}.json`];
+
+  return (
+    <RLayerVector<Feature<Geometry>>
+      zIndex={10}
+      features={
+        new GeoJSON({
+          dataProjection: 'EPSG:3857',
+          featureProjection: 'EPSG:3857',
+        }).readFeatures(boundary) as Feature<Geometry>[]
+      }
+    >
+      <RStyle.RStyle>
+        <RStyle.RStroke color="white" width={3} />
+        <RStyle.RFill color="transparent" />
+      </RStyle.RStyle>
+    </RLayerVector>
+  );
+};
 
 const Visualization = forwardRef((_props, ref: ForwardedRef<HTMLDivElement>) => {
   const [layers, setLayers] = useState<string[]>([]);
   const [boundaryShowing, setBoundaryShowing] = useState(true);
   const [ctrlHeld, setCTRLHeld] = useState(false);
+  const [city, setCity] = useState<ComboboxItem | null>(null);
+  const [view, setView] = useState(initial);
 
   function downHandler({ key }: KeyboardEvent) {
     if (key === 'Control') {
@@ -140,7 +178,13 @@ const Visualization = forwardRef((_props, ref: ForwardedRef<HTMLDivElement>) => 
   return (
     <div ref={ref} style={{ position: 'relative' }}>
       <div style={{ width: '100%', height: 'calc(100vh - 60px)', marginTop: 60 }}>
-        <RMap initial={{ center: stockholmCenter, zoom: 9 }} height="100%" noDefaultInteractions>
+        <RMap
+          initial={initial}
+          height="100%"
+          noDefaultInteractions
+          noDefaultControls
+          view={[view, setView]}
+        >
           <RDoubleClickZoom />
           <RDragPan />
           <RPinchRotate />
@@ -152,55 +196,63 @@ const Visualization = forwardRef((_props, ref: ForwardedRef<HTMLDivElement>) => 
             attributions="Source: Esri, Maxar, Earthstar Geographics, and the GIS User Community"
             projection="EPSG:3857"
           />
-          {boundaryShowing && (
-            <RLayerVector<Feature<Geometry>>
-              zIndex={10}
-              features={
-                new GeoJSON({
-                  dataProjection: 'EPSG:3857',
-                  featureProjection: 'EPSG:3857',
-                }).readFeatures(stockholmBoundary) as Feature<Geometry>[]
-              }
-            >
-              <RStyle.RStyle>
-                <RStyle.RStroke color="white" width={3} />
-                <RStyle.RFill color="transparent" />
-              </RStyle.RStyle>
-            </RLayerVector>
-          )}
-          <VisualizationLayers layers={layers} />
+          {boundaryShowing && city && <BounadryLayer city={city} />}
+          {city && <VisualizationLayers layers={layers} city={city.value} />}
         </RMap>
       </div>
       <div style={{ bottom: 20, left: 20, position: 'absolute' }}>
+        {city && (
+          <Paper
+            shadow="xs"
+            withBorder
+            p="xl"
+            style={{ width: '22em', maxWidth: 'calc(100vw - 40px)' }}
+          >
+            <Checkbox.Group
+              label={`Locate NbS in ${city.label}`}
+              description={`Taking ${city.label} as our study site, we identify the demands, locations, and types of NbS interventions that could maximize carbon reduction benefits.`}
+              value={layers}
+              onChange={setLayers}
+            >
+              {data.map((value) => (
+                <>
+                  <Space h="xs" key={`space_${value.value}`} />
+                  <Checkbox
+                    label={value.title}
+                    color={value.color}
+                    value={value.value}
+                    key={`checkbox_${value.value}`}
+                  />
+                </>
+              ))}
+            </Checkbox.Group>
+            <Space h="lg" />
+            <Switch
+              checked={boundaryShowing}
+              onChange={(event) => setBoundaryShowing(event.currentTarget.checked)}
+              label={`Show ${city.label} county boundary`}
+            />
+          </Paper>
+        )}
+      </div>
+      <div style={{ top: 20, left: 20, position: 'absolute' }}>
         <Paper
           shadow="xs"
           withBorder
           p="xl"
           style={{ width: '22em', maxWidth: 'calc(100vw - 40px)' }}
         >
-          <Checkbox.Group
-            label="Locate NbS in Stockholm"
-            description="Taking Stockholm as our study site, we identify the demands, locations, and types of NbS interventions that could maximize carbon reduction benefits."
-            value={layers}
-            onChange={setLayers}
-          >
-            {data.map((value) => (
-              <>
-                <Space h="xs" key={`space_${value.value}`} />
-                <Checkbox
-                  label={value.title}
-                  color={value.color}
-                  value={value.value}
-                  key={`checkbox_${value.value}`}
-                />
-              </>
-            ))}
-          </Checkbox.Group>
-          <Space h="lg" />
-          <Switch
-            checked={boundaryShowing}
-            onChange={(event) => setBoundaryShowing(event.currentTarget.checked)}
-            label="Show Stockholm county boundary"
+          <Select
+            data={cities}
+            placeholder="Pick a city!"
+            value={city ? city.value : null}
+            onChange={(_value, option: CitiesType) => {
+              setCity(option);
+              if (option.newView) {
+                setView({ center: option.newView.center, zoom: option.newView.zoom });
+              }
+            }}
+            allowDeselect={false}
           />
         </Paper>
       </div>
