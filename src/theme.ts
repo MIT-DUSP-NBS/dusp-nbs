82387c8b20715570f8e1e230d040ffc495f83c92
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-03-27 01:57:31 -0400

    switch back to old dark scheme

--- src/theme.ts
+++ src/theme.ts
@@ -13,10 +13,24 @@ const lightBlue: MantineColorsTuple = [
   '#007cb5',
 ];
 
+const dark: MantineColorsTuple = [
+  '#C1C2C5',
+  '#A6A7AB',
+  '#909296',
+  '#5c5f66',
+  '#373A40',
+  '#2C2E33',
+  '#25262b',
+  '#1A1B1E',
+  '#141517',
+  '#101113',
+];
+
 export const theme = createTheme({
   /** Put your mantine theme override here */
   primaryColor: 'light-blue',
   colors: {
     'light-blue': lightBlue,
+    dark,
   },
 });
